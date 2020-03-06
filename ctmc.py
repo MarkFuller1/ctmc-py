0bc5631c07d2d1c62ace11015b0d7edccf2f8513
MarkFuller1 <mark_fuller1@baylor.edu>
2020-03-06 13:50:31 -0600

    try db

--- ctmc.py
+++ ctmc.py
@@ -1,9 +1,13 @@
 from flask import Flask
-application = Flask(__name__)
+import dbconfig as cfg
+import pymysql
 
+application = Flask(__name__)
+con= pymysql.connect(cfg.mysql['host'], cfg.mysql['user'], cfg.mysql['password'], cfg.mysql['db'])
+    
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    application.run(debug=True, port="5000");
+    application.run(debug=True, port="5000")
---
+++ dbconfig.py
@@ -0,0 +1,5 @@
+mysql = { 'username': 'davidisMaster', 
+'host': 'dbmaininstance.ci9rmetj8mwt.us-east-1.rds.amazonaws.com',
+'password': 'Idaho1!',
+'db': 'dbmaininstance'
+}
