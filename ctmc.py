f5ea93389a33a8124581ddba44db179eeb88568d
david-b3ggs <david_beggs@baylor.edu>
2020-03-06 13:52:10 -0600

    Added test endpoint

--- ctmc.py
+++ ctmc.py
@@ -1,9 +1,16 @@
-from flask import Flask
+#THIS FILE HOUSES MAIN APPLICATION AND ENDPOINTS
+#COMPLEX CALCULATION AND DB QUERIES SHOULD BE MADE ELSEWHERE
+
+from flask import Flask, jsonify
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@application.route('/testGet', methods=['GET'])
+def retStuff():
+    return jsonify({'hello': 'howdy'})
 
 if __name__ == "__main__":
     application.run(debug=True, port="5000");
