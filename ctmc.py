3525307cc34baa39c080a558f8c3b53e427764a0
MarkFuller1 <mark_fuller1@baylor.edu>
2020-03-07 18:59:01 -0600

    clean up flask

--- ctmc.py
+++ ctmc.py
@@ -3,76 +3,86 @@
 
 from flask import Flask, jsonify
 import dbconfig as cfg
-import pymysql
-import json
 from flask_cors import cross_origin
+import Execute as e
 
 application = Flask(__name__)
 con = pymysql.connect(cfg.mysql['host'], cfg.mysql['user'], cfg.mysql['password'], cfg.mysql['db'])
 
 @application.route("/")
 @cross_origin()
-def hello():
+def home():
+    return "Hello! Welcome to the backend, please go to the frontend <a href=\"http://ctmc-js.herokuapp.com/\">here</a>"
+
+
+@application.route("/getAllTeams")
+@cross_origin(origin='*')
+def getAllTeams():
     with con:
-        cur = con.cursor()
-        cur.execute("select * from Teams")
-        results = cur.fetchall()
+        res = e.execute(con, "select name from Teams")
+
+        print(res)
+
+        return res
 
 
 @application.route("/getAllPlayers",  methods=['GET'])
 @cross_origin(origin='*')
 def getAllBatters():
     with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM Batting")
+        res = e.execute(con, "SELECT concat(nameFirst, ' ', nameLast) as name FROM People order by nameLast")
 
-        row_headers = [x[0] for x in cur.description]  # this will extract row headers
-        rv = cur.fetchall()
-        json_data = []
-        for result in rv:
-            json_data.append(dict(zip(row_headers, result)))
-        return json.dumps(json_data, indent=4, sort_keys=True, default=str)
+        print(res)
+
+        return res
+
 
 @application.route('/<playerID>/generalData', methods=['GET'])
 @cross_origin(origin='*')
 def getGeneralData(playerID):
     with con:
-        cur = con.cursor()
-        cur.execute("select * from People where playerID = %s", (playerID))
-        field_names = [i[0] for i in cur.description]
-        results = cur.fetchall()
+        filtered = "'" + playerID + "'"
+        res = e.execute(con, "select * from People where playerID = " + filtered + ";")
 
-    return jsonify({'colnames': field_names,
-                    'generalData': results})
+        print(res)
+
+        return res
 
 
 @application.route('/<playerID>/battingData', methods=['GET'])
 @cross_origin(origin='*')
 def getBattingData(playerID):
     with con:
-        cur = con.cursor()
-        cur.execute("select * from Batting where playerID = %s", (playerID))
-        results = cur.fetchall()
-    return jsonify({'data': results})
+        filtered = "'" + playerID + "'"
+        res = e.execute(con, "select * from Batting where playerID = " + filtered + ";")
+
+        print(res)
+
+        return res
 
 
 @application.route('/<playerID>/pitchingData', methods=['GET'])
 @cross_origin(origin='*')
 def getPitchingData(playerID):
     with con:
-        cur = con.cursor()
-        cur.execute("select * from Pitching where playerID = %s", (playerID))
-        results = cur.fetchall()
-    return jsonify({'data': results})
+        filtered = "'" + playerID + "'"
+        res = e.execute(con, "select * from Pitching where playerID = " + filtered + ";")
+
+        print(res)
+
+        return res
+
 
 @application.route('/<playerID>/fieldingData', methods=['GET'])
 @cross_origin(origin='*')
 def getFieldingData(playerID):
     with con:
-        cur = con.cursor()
-        cur.execute("select * from Fielding where playerID = %s", (playerID))
-        results = cur.fetchall()
-    return jsonify({'data': results})
+        filtered = "'" + playerID + "'"
+        res = e.execute(con, "select * from Fielding where playerID = " + filtered + ";")
+
+        print(res)
+
+        return res
 
 if __name__ == "__main__":
     application.run(debug=True, port="5000")
